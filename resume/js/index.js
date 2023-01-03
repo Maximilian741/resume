a165a49379b0fc724e1483e0039d89788f375020
Max Casteel <maximilian.casteel@umconnect.umt.edu>
2023-01-02 22:05:55 -0700

    Major changes to the resume website. Added a new background image, added a new image to the portfolio section, and added a new javascript file to the website.

---
+++ resume/js/index.js
@@ -0,0 +1,3 @@
+//Max Casteel
+//This is the javascript file for the resume website, it contains the functions for the contact form.
+//I also plan to use this file to add increased functionality to the website in the future once I come up with more ideas.
